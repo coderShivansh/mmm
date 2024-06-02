c90836675897012d53693aeed85a9d16bd201e53
coderShivansh <codershivansh@gmail.com>
2024-06-02 19:16:42 +0530

    added metadata

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,21 +1,18 @@
-'use client'
+export const metadata = {
+  title: 'MakeMyMvp Home',
+  description: 'Home Page',
+}
 
-// import { useState } from 'react';
 import Hero from '@/components/hero';
 import Features from '@/components/features';
 import FeaturesBlocks from '@/components/features-blocks';
 import Testimonials from '@/components/testimonials';
 import Newsletter from '@/components/newsletter';
-// import CalendlyEmbed from '@/components/calendlyEmbed';
 import Header from '@/components/ui/header';
 
 
 export default function Home() {
-  // const [showCalendly, setShowCalendly] = useState(false);
-
-  // const handleBookCallClick = () => {
-  //   setShowCalendly(true);
-  // };
+ 
 
   return (
     <>
--- components/hero.tsx
+++ components/hero.tsx
@@ -69,6 +69,7 @@ export default function Hero() {
                     href="https://calendly.com/codershivansh/30min"
                     target="_blank"
                     className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0"
+                    rel="noreferrer"
                   >
                     Book a Call
                   </Link>

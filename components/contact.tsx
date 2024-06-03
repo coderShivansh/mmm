0125866f5247ac8268dbb5bc3d17505e0338ad0e
coderShivansh <codershivansh@gmail.com>
2024-06-03 16:32:39 +0530

    removed autofocus on phone number in contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -65,8 +65,8 @@ const Contact = () => {
               inputClass="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               inputProps={{
                 name: 'phone',
-                required: true,
-                autoFocus: true,
+                required: false,
+                autoFocus: false,
               }}
             />
           </div>
